a1ada0f64288db7bf2362dcd748be2f24b5db9a8
franshiro <franshiro@gmail.com>
2018-07-19 03:24:22 +0700

    merubah susunan huruf dalam kalimat

--- exercise9.js
+++ exercise9.js
@@ -1,12 +1,14 @@
 function tukarBesarKecil(kalimat) {
     // you can only write your code here!
-    var hurufBesar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var hurufBesar = '0123456789-+ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     var hurufKecil = hurufBesar.toLowerCase()
     var arr = kalimat.split(' ')
     var tampung = []
+    var joinTemp = []
 
     
     for(var a = 0; a<arr.length; a++){
+        
         tampung.push([])
         for(var b = 0; b < arr[a].length ; b++){
             
@@ -14,25 +16,27 @@ function tukarBesarKecil(kalimat) {
             for(var j = 0; j<hurufBesar.length; j++){
                 // console.log(hurufBesar[j])
                 if(arr[a][b] === hurufBesar[j]){
-                    console.log(hurufBesar[j].toLowerCase())
+                    // console.log(hurufBesar[j].toLowerCase())
                     tampung[a].push(hurufBesar[j].toLowerCase())
                 }
                 else if(arr[a][b] === hurufKecil[j]){
-                    console.log(hurufBesar[j])
+                    // console.log(hurufBesar[j])
                     tampung[a].push(hurufBesar[j])
 
                 }
             }
-        
+            
         }
         
+        joinTemp.push(tampung[a].join(''))
+        // console.log(joinTemp)
     }
-    console.log(tampung)
+    return joinTemp.join(' ')
   }
   
   // TEST CASES
   console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
-//   console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
-//   console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
-//   console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
-//   console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
+  console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+  console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+  console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+  console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
