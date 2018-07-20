21127ed608cb0a36af2fbe64cc0d898e7210c8f7
franshiro <franshiro@gmail.com>
2018-07-20 14:59:57 +0700

    memasukkan array ke dalam obj

--- exercise11.js
+++ exercise11.js
@@ -2,29 +2,36 @@ function changeMe(arr) {
     // you can only write your code here!
     // console.log(arr.length)
     var strObj = ['firstName', 'lastName', 'gender', 'age']
-    var hasilObj = new Object
+    var hasilObj = {}
     // var namaObj = arr[i][0] + ' ' +arr[i][1]
     for(var i = 0; i<arr.length; i++){
-        console.log(arr[i])
-        // hasilObj.(arr[i][0] + ' ' +arr[i][1]) = 
+        console.log((i+1) + '.' + arr[i][0], arr[i][1])
+        // hasilObj[(i+1) + '.' + arr[i][0], arr[i][1]] = 'hasil'
         for(var j = 0; j < strObj.length; j++){
             // console.log(strObj[j])
+            
             for(var k = j; k< strObj.length; k++){
                 if(j === k && strObj[j] !== 'age'){
-                    console.log(strObj[j], ' : ', arr[i][k])
+                    // console.log(strObj[j], ' : ', arr[i][k])
+                    hasilObj[strObj[j]] = arr[i][k] 
                                       
                 }
                 else if(strObj[j] === 'age' && arr[i][k] < 2018){
-                    console.log(strObj[j], ' : ', (2018 - arr[i][k]))
+                    // console.log(strObj[j], ' : ', (2018 - arr[i][k]))
+                    hasilObj[strObj[j]] = (2018 - arr[i][k]) 
                 }
                 else if(strObj[j] === 'age' && arr[i][k] === undefined  ){
-                    console.log(strObj[j], ' : ', 'invalid')
+                    // console.log(strObj[j], ' : ', 'invalid')
+                    hasilObj[strObj[j]] = 'invalid'
                 }
                 else if(strObj[j] === 'age' &&  arr[i][k] >= 2018 ){
-                    console.log(strObj[j], ' : ', 'invalid')
+                    // console.log(strObj[j], ' : ', 'invalid')
+                    hasilObj[strObj[j]] = 'invalid'
                 }
             }
         }
+
+    console.log(hasilObj)
         
     }
 
@@ -42,4 +49,4 @@ function changeMe(arr) {
   //   lastName: 'Downey',
   //   gender: 'Male',
   //   age: 'Invalid Birth Year' }
-//   changeMe([]); // ""
+  changeMe([]); // ""
